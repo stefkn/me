c450e25d100739bd2153601396f8e412600a8fdf
Stefan Kenichiro Nowak <stefan.k.nowak@gmail.com>
2026-03-12 12:49:17 +0000

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://stefkn.com',
+  site: 'https://stefkn.github.io/personal-site/',
   prefetch: {
     prefetchAll: true
   },
